1180c6401122b55e03367bc921e1d5458ec30216
ed <edchapman88@gmail.com>
2021-05-08 15:32:20 +0100

    Working analogue sensor output in PlatformIO project.

---
+++ hardware/c_lang/PlatformIO/src/main.cpp
@@ -0,0 +1,37 @@
+/**
+ * Blink
+ *
+ * Turns on an LED on for one second,
+ * then off for one second, repeatedly.
+ */
+#include "Arduino.h"
+
+int sensor_pin = A0;
+
+int output_value ;
+
+void setup() {
+
+   Serial.begin(9600);
+
+   Serial.println("Reading From the Sensor ...");
+
+   delay(2000);
+
+   }
+
+void loop() {
+
+   output_value= analogRead(sensor_pin);
+
+   output_value = map(output_value,550,0,0,100);
+
+   Serial.print("Mositure : ");
+
+   Serial.print(output_value);
+
+   Serial.println("%");
+
+   delay(1000);
+
+   }
